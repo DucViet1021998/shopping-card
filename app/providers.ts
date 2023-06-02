abd24a42e2fe2d8f435e9f3544dd991c858962e3
DucViet1021998 <viethg1021998@gmail.com>
2023-06-02 17:04:10 +0700

    cm7

---
+++ app/providers.ts
@@ -0,0 +1,13 @@
+// 'use client';
+
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistStore } from 'redux-persist';
+// import store from '@/components/store';
+// const persist = persistStore(store);
+
+// export function Providers({ children }) {
+//     return (
+
+//     );
+// }
